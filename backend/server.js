e3ef985e52e774d7d8b573915445ca3b566021a9
Pavan Ganesh <pavanganesh2804@gmail.com>
2025-07-14 17:08:13 +0530

    Fix: use bcryptjs & update deploy configs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
